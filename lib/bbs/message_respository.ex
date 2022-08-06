c679c7c3c07703901d2ee0ef275ff91757ef00dd
Jonathan Arnett <jonarnett90@gmail.com>
2022-08-06 11:35:24 -0400

    Terser Agent version

--- lib/bbs/message_respository.ex
+++ lib/bbs/message_respository.ex
@@ -1,36 +1,19 @@
 defmodule BBS.MessageRepository do
   use Agent
 
-  # Server
-
-  def init(messages) do
-    messages
-  end
-
-  def handle_post(messages, message) do
-    [message | messages]
-  end
-
-  def handle_read_all(messages) do
-    messages
-  end
-
-  # Client
-
   def post(message) do
-    Agent.update(BBS.MessageRepository, BBS.MessageRepository, :handle_post, [message])
+    Agent.update(BBS.MessageRepository, fn messages ->
+      [message | messages]
+    end)
   end
 
   def read_all() do
-    Agent.get(BBS.MessageRepository, BBS.MessageRepository, :handle_read_all, [])
+    Agent.get(BBS.MessageRepository, fn messages ->
+      messages
+    end)
   end
 
   def start_link(messages) do
-    Agent.start_link(
-      BBS.MessageRepository,
-      :init,
-      [messages],
-      name: BBS.MessageRepository
-    )
+    Agent.start_link(fn -> messages end, name: BBS.MessageRepository)
   end
 end
